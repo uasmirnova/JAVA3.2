9743014278c79f2934217a2650e9b4b5e10319f5
uasmirnova <uasmirnova21@yandex.ru>
2022-07-21 15:51:18 +0300

    calculate v2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
-    public float calculate(float weight, float height) {
-        float result = weight / ( height * height );
-        return result;
+    public float calculate(int weight, float height) {
+        return weight / ( height * height );
     }
 
 }
